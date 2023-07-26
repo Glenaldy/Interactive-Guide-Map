aff9302f89734aa2f17dac31499a35929fc3147b
Glenaldy <glenaldy98@yahoo.com>
2023-07-27 05:23:03 +0900

    working build

--- src/hooks/useDatabase.tsx
+++ src/hooks/useDatabase.tsx
@@ -1,24 +1,33 @@
 import {useEffect, useState} from "react";
 
 function useDatabase<T>(source: string, type: new (...args: any[]) => T): [Array<T>, boolean] {
-    const fetchData = async () =>
-        await fetch(source)
-            .then(res => res.json())
-            .then(data => {
-                return data
-            })
+    const fetchData = async () => {
+        try {
+            const res = await fetch(source);
+            if (!res.ok) {
+                throw new Error("Network response was not ok");
+            }
+            return await res.json();
+        } catch (error) {
+            throw new Error("Error fetching data");
+        }
+    };
 
-    const [database, setDatabase] = useState(Array<T>());
-    const [isLoading, setIsLoading] = useState(true)
+    const [database, setDatabase] = useState<Array<T>>([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
-        fetchData().then(r => {
-            setDatabase(r);
-            setIsLoading(false);
-        })
+        fetchData()
+            .then((r) => {
+                setDatabase(r);
+                setIsLoading(false);
+            })
+            .catch((error) => {
+                setIsLoading(false);
+            });
     }, [source]);
 
-    return [database, isLoading]
+    return [database, isLoading];
 }
 
 export default useDatabase;
