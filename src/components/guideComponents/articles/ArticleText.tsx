d48862fceefedfd5115e19c32cb5f45c0e587b37
Glenaldy <glenaldy98@yahoo.com>
2023-07-28 08:33:24 +0900

    Added logo

--- src/components/guideComponents/articles/ArticleText.tsx
+++ src/components/guideComponents/articles/ArticleText.tsx
@@ -13,8 +13,9 @@ const ArticleText = ({currentArticle}: Props) => {
     return (
         <div className={"article-text"}>
             <div className="current-article-body">
-                {reactStringReplace(currentArticle.content, /({[a-zA-Z0-9_]+})/g, (match, i) => {
+                {reactStringReplace(currentArticle.content, /({place_[a-zA-Z_-]*})/g, (match, i) => {
                     const placeReference = currentArticle?.placeReferences.find(place => place.reference === match)
+                    console.log(match)
                     return placeReference ?
                         <SpecialText
                             key={`place-span-key_${placeReference.reference}`}
